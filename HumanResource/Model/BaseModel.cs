ca7cf9b59ff1867081f86952bcd2e8e1d3e650e8
quangsmile <anhquanghoang@outlook.com>
2019-06-25 09:04:15 +0700

    Tạo lại

---
+++ HumanResource/Model/BaseModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HumanResource.Model
+{
+    [NotMapped]
+    class BaseModel
+    {
+        [Key]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+    }
+
+    [NotMapped]
+    class BaseModel2
+    {
+        [Key]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public DateTime CRUDDate { get; set; }
+        public DateTime CRUDBy { get; set; }
+
+    }
+}
---
+++ HumanResource/frmMain.cs
@@ -0,0 +1,82 @@
+﻿using DevExpress.XtraBars.Ribbon;
+using DevExpress.XtraEditors;
+using DevExpress.XtraTab;
+using HumanResource.Recruitment;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HumanResource
+{
+    public partial class frmMain : RibbonForm
+    {
+        public frmMain()
+        {
+            this.LookAndFeel.UseDefaultLookAndFeel = false;
+            this.LookAndFeel.SetSkinStyle("Visual Studio 2013 Light");
+            InitializeComponent();
+        }
+
+        private void btnYeuCauTuyenDung_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            XtraForm f = new frmRecruitmentRequirement();
+            CheckTabOpenDev(f);
+
+        }
+
+        // Sử dụng kiểm tra trước khi add mới XtraTabPage (Đã add control)
+        // Nếu XtraTabPage đã được add thì không add trùng 
+        //---------------- chưa được add thì add mới XtraTabPage
+        public void CheckTabOpenDev(XtraForm _f)
+        {
+            if (xtraTabMain.TabPages.Count == 0)
+            {
+                AddControl(_f);
+            }
+            else
+            {
+                bool trunglap = false;
+                for (int i = 0; i < xtraTabMain.TabPages.Count; i++)
+                {
+                    if (("Tab" + _f.Name) == xtraTabMain.TabPages[i].Name)
+                    {
+                        trunglap = true;
+                        xtraTabMain.SelectedTabPage = xtraTabMain.TabPages[i];
+                        break;
+                    }
+                }
+                if (trunglap == false) AddControl(_f);
+            }
+        }
+
+        // Sử dụng add Form vào trong XtraTabPage (Bộ công cụ DevExpress)
+        // Tham số truyền vào (XtraForm)
+        public void AddControl(XtraForm _f)
+        {
+            var _Tab = new XtraTabPage();
+            xtraTabMain.TabPages.Add(_Tab);
+            xtraTabMain.SelectedTabPage = _Tab;
+            _Tab.Name = "Tab" + _f.Name;
+            _Tab.Text = _f.Text;
+
+            // Dinh dang form
+            _f.TopLevel = false;
+            _f.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+
+            // Add Form
+            _Tab.Controls.Add(_f);
+
+            // Fill form toan bo Tab 
+            _f.Dock = DockStyle.Fill;
+
+            //Hien thi
+            _f.Show();
+        }
+    }
+}
