9d4d42ff7cca83278f18455c7d3f31e7b23581f8
DoanTrang <doantrang483@gmail.com>
2019-06-25 08:59:28 +0700

    Create Project

---
+++ Recruitment/frmRecruitmentRequirement.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace HumanResource.Recruitment
+{
+    partial class frmRecruitmentRequirement
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grRR = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            ((System.ComponentModel.ISupportInitialize)(this.grRR)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grRR
+            // 
+            this.grRR.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grRR.Location = new System.Drawing.Point(8, 338);
+            this.grRR.MainView = this.gridView1;
+            this.grRR.Name = "grRR";
+            this.grRR.Size = new System.Drawing.Size(1366, 599);
+            this.grRR.TabIndex = 0;
+            this.grRR.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.GridControl = this.grRR;
+            this.gridView1.Name = "gridView1";
+            this.gridView1.OptionsView.ShowGroupPanel = false;
+            // 
+            // frmRecruitmentRequirement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1386, 949);
+            this.Controls.Add(this.grRR);
+            this.Name = "frmRecruitmentRequirement";
+            this.Text = "Yêu cầu tuyển dụng";
+            ((System.ComponentModel.ISupportInitialize)(this.grRR)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraGrid.GridControl grRR;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+    }
+}
---
+++ Recruitment/frmRecruitmentRequirement.cs
@@ -0,0 +1,21 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HumanResource.Recruitment
+{
+    public partial class frmRecruitmentRequirement : XtraForm
+    {
+        public frmRecruitmentRequirement()
+        {
+            InitializeComponent();
+        }
+    }
+}
